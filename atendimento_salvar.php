f42b1707a25ab71a65e11051dc4e5be53e78c6b5
CarlosAlbertoCode <alberto2007s10@gmail.com>
2024-10-02 08:40:25 -0300

    Foi inserido um agendamento

--- atendimento_salvar.php
+++ atendimento_salvar.php
@@ -23,7 +23,30 @@ and open the template in the editor.
 
         <form name="formServico" action="" method="post">
             <div id="body">
+                <?php 
+                //Passo 01: Incluir as configurações de acesso a dados
+                    include "conexao_bd.php";
+                // Passo 02: Capturar os dados digitados
+                $nome = $_POST["txtNome"];
+                $id_servico = $_POST["selectServico"];
+                $data_agendamento = $_POST["txtData"];
+                $horario_agendamneto = $_POST["selectHorario"];
 
+                // Passo 03: Montar o comando SQL para inserir
+                $sql = "INSERT INTO agendamento(nome,id_servico,data_agendamento,horario_agendamneto) ";
+                $sql .= " VALUES('$nome','$id_servico','$data_agendamento','$horario_agendamneto')";
+                
+                // Passo 04: Executar no BDA o comando
+                if (executarComando($sql))
+                {
+                    echo "<h2> Serviço agendado!</h2>";
+                } 
+                else
+                {
+                    echo "<h2> Não foi possível agendar!</h2>";
+                }
+
+                ?>
                 
             </div>
         </form>
